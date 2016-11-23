4794843aef294e049bb694ecd1a40e90273a723e
chris-conklin <cconklin2376@gmail.com>
2016-11-23 11:25:51 -0500

    Added install script to create default directories if not exists

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# to create directories for use
+# fix later to check for existence
+
+create_directory()
+{
+
+  if [[ ! -e $1 ]]; then
+    mkdir $1
+  elif [[ ! -d $1 ]]; then
+    echo "$1 already exists but is not a directory" 1>&2
+  fi
+}
+
+create_directory "content"
+create_directory "db"
+create_directory "log"
+create_directory "dropoff"
+create_directory "trashes"
+
+
+
